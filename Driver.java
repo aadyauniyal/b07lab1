7da50273216f43f065af2d5da04bc15eb743c39e
Aadya Uniyal <aadyauniyal@gmail.com>
2024-09-29 21:54:44 -0400

    modified Polynomial.java and Driver.java

--- Driver.java
+++ Driver.java
@@ -1,16 +1,16 @@
+package b07lab;
+
 public class Driver {
-public static void main(String [] args) {
-Polynomial p = new Polynomial();
-System.out.println(p.evaluate(3));
-double [] c1 = {6,0,0,5};
-Polynomial p1 = new Polynomial(c1);
-double [] c2 = {0,-2,0,0,-9};
-Polynomial p2 = new Polynomial(c2);
-Polynomial s = p1.add(p2);
-System.out.println("s(0.1) = " + s.evaluate(0.1));
-if(s.hasRoot(1))
-System.out.println("1 is a root of s");
-else
-System.out.println("1 is not a root of s");
-}
+    public static void main(String[] args) {
+        double[] c1 = {6, 0, 0, 5}; 
+        int[] p1 = {0, 1, 2, 3}; 
+        Polynomial polynomial1 = new Polynomial(c1, p1);
+        double[] c2 = {0, -2, 0, 0, -9}; 
+        int[] p2 = {0, 1, 2, 3, 4}; 
+        Polynomial polynomial2 = new Polynomial(c2, p2); 
+        Polynomial sum = polynomial1.add(polynomial2);
+        System.out.println("Sum of polynomial1 and polynomial2: " + sum);
+        Polynomial product = polynomial1.multiply(polynomial2);
+        System.out.println("Product of polynomial1 and polynomial2: " + product);
+    }
 }
--- Polynomial.java
+++ Polynomial.java
@@ -1,40 +1,104 @@
+package b07lab;
+import java.util.Scanner;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileWriter;
+import java.io.IOException;
+
 
 class Polynomial{
 
-double [] arr;
+double [] coeff;
+int [] power;
 
 Polynomial()
 {
-	this.arr=new double[0];
+	this.coeff=new double[0];
+	this.power=new int[0];
 }
 
-Polynomial(double ar[])
-{ arr = ar;
+Polynomial(double co[], int po[])
+{  coeff=co;
+	power=po;
+}
+
+Polynomial(File note) throws FileNotFoundException 
+{Scanner sc=new Scanner(note);
+String str=sc.nextLine();
+String[] poly=str.split("+-");
+int len=poly.length;
+for(int i=0;i<poly.length;i++)
+{	String term=poly[i];
+	int index=poly[i].indexOf('x');
+	if(index==-1)
+	{
+		power[i]=0;
+		coeff[i]=Double.parseDouble(poly[i]);
+			
+	}
+	else
+	{String coef = poly[i].substring(0, index);
+    if (coef==" ") {
+        coeff[i] = 1; 
+    }  else {
+        coeff[i] = Double.parseDouble(coef); 
+    }
+
+    if (poly[i].substring(0, index)!="") {
+        power[i] = Integer.parseInt(poly[i].substring(index + 1).trim()); 
+        coeff[i] = Double.parseDouble(poly[i].substring(0, index).trim()); 
+    } else {
+        power[i] = 1; 
+        coeff[i] = Double.parseDouble(poly[i].substring(0, index).trim()); 
+    }
+
+}
+}
+sc.close();}
+
+
+public int findmax(int a[])
+{  int max=a[0]; 
+	for(int i=0;i<a.length;i++)
+	{
+		if(a[i]>max)
+		{
+			max=a[i];
+		}
+	}
+	return max;
 }
 
 public Polynomial add(Polynomial poly)
 {
  
-int total_len = poly.arr.length;
-if(this.arr.length>poly.arr.length)
- total_len=this.arr.length;
-double [] added= new double[total_len];
-for (int i = 0; i < total_len; i++) {
-	if (i < this.arr.length && i < poly.arr.length)
-		added[i] = this.arr[i] + poly.arr[i];
-    else if (i >= this.arr.length) 
-	added[i] = poly.arr[i];
-    else
-	added[i] = this.arr[i];
+int max1 = findmax(this.power);
+int max2 = findmax(poly.power);
+int totallen = max1;
+if (totallen < max2) totallen = max2;
+    double[] addCoeff = new double[totallen+1]; 
+    int[] addPower = new int[totallen+1];
+	int i = 0; 
+	while(i <= totallen)  {
+    addPower[i] = i;  
+    addCoeff[i] = 0;  
+    i++;
+    }
+    for ( i = 0; i <= totallen; i++) {
+    if (i < poly.power.length && poly.power[i] == addPower[i]) {
+        addCoeff[i] += poly.coeff[i]; 
         }
-return new Polynomial(added);
+    if (i < this.power.length && this.power[i] == addPower[i]) {
+        addCoeff[i] += this.coeff[i]; 
+    }}
+    return new Polynomial(addCoeff, addPower);
 }
 
 public double evaluate(double x)
 {
 double sum=0;
-for(int i=0;i<arr.length;i++)
-{ sum = sum + arr[i]* Math.pow(x,i);
+for(int i=0;i<coeff.length;i++)
+{ sum = sum + coeff[i]* Math.pow(x,power[i]);
 }
 return sum;
 }
@@ -45,9 +109,48 @@ if(evaluate(x)==0) return true;
 else return false;
 }
 
+Polynomial multiply(Polynomial poly) {
+	int max1 = findmax(this.power);
+	int max2 = findmax(poly.power);
+	int totallen = max1+max2;
+	double[] mulCoeff = new double[totallen+1]; 
+    int[] mulPower = new int[totallen+1];
+	int i = 0; 
+	while(i <= totallen)  {
+    mulPower[i] = i;  
+    mulCoeff[i] = 0;  
+    i++;
+    }
+    for (i = 0; i < this.power.length; i++) {
+        for (int j = 0; j < poly.power.length; j++) {
+            if (this.power[i] + poly.power[j] == mulPower[this.power[i] + poly.power[j]]) {
+                mulCoeff[this.power[i] + poly.power[j]] += this.coeff[i] * poly.coeff[j];
+            }
+        }
+    }
+
+	return new Polynomial(mulCoeff, mulPower);
 }
 
+public void saveToFile(String str) throws IOException{
+	FileWriter output= new FileWriter(str, true);
+	for(int i=0;i<power.length;i++) {
+		if(coeff[i]!=0 && power[i]!=0 && i!=power.length-1) {
+			
+		}
+		if(coeff[i]!=0 && power[i]==0)
+		{output.write(Double.toString(coeff[i]));
+		}
+		if(coeff[i]!=0 && power[i]!=0 && i!=power.length-2)
+		{	String s= Double.toString(coeff[i])+"x"+Double.toString(power[i]);
+        if (i > 0 && coeff[i] > 0) {
+            output.write("+");
+        }
 
-
-
-
+        output.write(s); 
+		}
+		
+	}
+	output.close();
+}
+}
